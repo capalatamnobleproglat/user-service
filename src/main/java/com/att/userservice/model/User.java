e49bd128d057c1b75c374864b857d580072001f6
capalatamnobleproglat <capalatam@nobleproglat.onmicrosoft.com>
2023-08-17 18:52:26 +0000

    cambio 1

---
+++ src/main/java/com/att/userservice/model/User.java
@@ -0,0 +1,45 @@
+package com.att.userservice.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+
+    public User() {
+    }
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String firstName) {
+        this.username = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
