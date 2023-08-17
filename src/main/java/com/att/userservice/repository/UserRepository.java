1f549e1bf2511677697e8c9f669721e30c0cc226
capalatamnobleproglat <capalatam@nobleproglat.onmicrosoft.com>
2023-08-17 05:29:21 +0000

    setting the project for first day

--- src/main/java/com/att/userservice/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.att.userservice.repository;
-
-import com.att.userservice.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
