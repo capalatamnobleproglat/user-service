25c31cab6b519e8f3b8f54970013dbf776e56e55
capalatamnobleproglat <capalatam@nobleproglat.onmicrosoft.com>
2023-08-31 23:09:29 +0000

    cambios user

---
+++ src/main/java/com/att/userservice/controller/UserController.java
@@ -0,0 +1,49 @@
+package com.att.userservice.controller;
+
+import com.att.userservice.dto.UserDto;
+import com.att.userservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<UserDto> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping
+    public UserDto createUser(@RequestBody UserDto userDto) {
+        return userService.createUser(userDto);
+    }
+
+    @GetMapping("/{id}")
+    public UserDto getUserById(@PathVariable Long id) throws ChangeSetPersister.NotFoundException {
+        return userService.getUserById(id);
+    }
+
+    @PutMapping("/{id}")
+    public UserDto updateUser(@PathVariable Long id, @RequestBody UserDto userDto) throws ChangeSetPersister.NotFoundException {
+        return userService.updateUser(id, userDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) throws ChangeSetPersister.NotFoundException {
+        userService.deleteUser(id);
+    }
+}
